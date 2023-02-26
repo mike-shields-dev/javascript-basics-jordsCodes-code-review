7550901c2d86749831a4caa12fbe5485ce63c8af
Jordan <j.r.noble@live.co.uk>
2023-02-26 16:04:08 +0000

    one function passed

--- src/__tests__/booleans.test.js
+++ src/__tests__/booleans.test.js
@@ -17,14 +17,14 @@ const {
 } = require('../booleans');
 
 describe('negate', () => {
-  it('returns the opposite of the passed boolean value', () => {
+  xit('returns the opposite of the passed boolean value', () => {
     expect(negate(true)).toBe(false);
     expect(negate(false)).toBe(true);
   });
 });
 
 describe('both', () => {
-  it('returns true if both of the given booleans are true', () => {
+  xit('returns true if both of the given booleans are true', () => {
     expect(both(true, true)).toBe(true);
     expect(both(true, false)).toBe(false);
     expect(both(false, true)).toBe(false);
@@ -33,7 +33,7 @@ describe('both', () => {
 });
 
 describe('either', () => {
-  it('returns true if at least one of the given booleans are true', () => {
+  xit('returns true if at least one of the given booleans are true', () => {
     expect(either(true, true)).toBe(true);
     expect(either(true, false)).toBe(true);
     expect(either(false, true)).toBe(true);
@@ -42,7 +42,7 @@ describe('either', () => {
 });
 
 describe('none', () => {
-  it('returns true if neither of the given booleans are true', () => {
+  xit('returns true if neither of the given booleans are true', () => {
     expect(none(true, true)).toBe(false);
     expect(none(true, false)).toBe(false);
     expect(none(false, true)).toBe(false);
@@ -51,7 +51,7 @@ describe('none', () => {
 });
 
 describe('one', () => {
-  xit('returns true if exactly one of the given booleans are true', () => {
+  it('returns true if exactly one of the given booleans are true', () => {
     expect(one(true, true)).toBe(false);
     expect(one(true, false)).toBe(true);
     expect(one(false, true)).toBe(true);
